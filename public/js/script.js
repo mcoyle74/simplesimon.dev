b4ed93f2842e2ef6cca07f4b5203ce633390dc5d
Michael Coyle <mcoyle74@gmail.com>
2016-06-21 16:06:26 -0500

    Changed class of divs from square to squares. Attempted to attach a JS animation to Play button but it doesn't work :(

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
-"use strict";
+'use strict';
+
+var pressedPlay = $('#play');
+var squares = $('.squares');
+
+pressedPlay.click(function() {
+	squares.forEach(function(square) {
+		square.animate({
+			height: '+=5px',
+			width: '+=5px'
+		}, 500)
+	});
+});
