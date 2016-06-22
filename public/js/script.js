ed0406db9be7bd520f46c162e254e56974384d38
Michael Coyle <mcoyle74@gmail.com>
2016-06-22 10:23:19 -0500

    Created function, getRandomSquare(), that randomly selects a square and it is called by pressPlay.click().

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,26 @@
 'use strict';
 
-var pressedPlay = $('#play');
-var squares = $('.squares');
+$(document).ready(function () {
+	var pressPlay = $('#play');
+	var red = $('#red');
+	var yellow = $('#yellow');
+	var green = $('#green');
+	var blue = $('#blue');
 
-pressedPlay.click(function(event) {
-	for (var i = 0; i < squares.length; i++) {
-		squares[i].animate({
-			height: '+=5px',
-			width: '+=5px'
-		}, 500);
-		squares[i].animate({
-			height: '-=5px',
-			width: '-=5px'
-		}, 500);
+	function getRandomSquare() {
+		var index = Math.floor(Math.random() * 4);
+		if (index == 0) {
+			red.fadeOut(250).fadeIn(250);
+		} else if (index == 1) {
+			yellow.fadeOut(250).fadeIn(250);
+		} else if (index == 2) {
+			green.fadeOut(250).fadeIn(250);
+		} else if (index == 3) {
+			blue.fadeOut(250).fadeIn(250);
+		}
 	}
+	
+	pressPlay.click(function() {
+		getRandomSquare();
+	});
 });
