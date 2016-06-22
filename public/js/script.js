3613e185808a58eff1ab64ea2f3abb68b63bd58d
Michael Coyle <mcoyle74@gmail.com>
2016-06-22 14:22:48 -0500

    Created .click() that records player sequence.

--- public/js/script.js
+++ public/js/script.js
@@ -8,32 +8,56 @@
 	var green = $('#green');
 	var blue = $('#blue');
 	var squares = $('.squares');
-	var control = $('#control')
-	var sequence = [];
-	var round = 0;
+	var control = $('#control');
+
+	var simon = {
+		sequence: [],
+		round: 0,
+		animateSequence: function() {
+			// animate
+		},
+		check: function() {
+			// if (this.sequence[i])...
+		}
+	}
+
+	var player = {
+		sequence: [],
+		rounds: 0,
+	}	
 
 	function getRandomSquare() {
-		var index = Math.floor(Math.random() * 4);
-		if (index == 0) {
+		var integer = Math.floor(Math.random() * 4) + 1;
+		if (integer == 1) {
 			red.fadeOut(250).fadeIn(250);
-			sequence.push(index);
-		} else if (index == 1) {
+			simon.sequence.push(integer);
+		} else if (integer == 2) {
 			yellow.fadeOut(250).fadeIn(250);
-			sequence.push(index);
-		} else if (index == 2) {
+			simon.sequence.push(integer);
+		} else if (integer == 3) {
 			green.fadeOut(250).fadeIn(250);
-			sequence.push(index);
-		} else if (index == 3) {
+			simon.sequence.push(integer);
+		} else if (integer == 4) {
 			blue.fadeOut(250).fadeIn(250);
-			sequence.push(index);
+			simon.sequence.push(integer);
 		}
-		round++;
-		instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + round + '</h3>');
-		$('#play').attr('hidden', true);
+		simon.round++;
+		instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + simon.round + '</h3>');
+		pressPlay.attr('hidden', true);
 	}
 	
-	squares.click(function() {
-		
+	$('[data-id]').click(function(event) {
+		var squarePushed = event.target.dataset.id;
+		console.log(squarePushed);
+		if (squarePushed == 1) {
+			player.sequence.push(squarePushed);
+		} else if (squarePushed == 2) {
+			player.sequence.push(squarePushed);
+		} else if (squarePushed == 3) {
+			player.sequence.push(squarePushed);
+		} else if (squarePushed == 4) {
+			player.sequence.push(squarePushed);
+		} 
 	});
 
 	pressPlay.click(function() {
