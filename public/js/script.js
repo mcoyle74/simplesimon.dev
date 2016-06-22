5796422f37f99929c721ae912581b13577dfc43c
Michael Coyle <mcoyle74@gmail.com>
2016-06-22 11:36:08 -0500

    JS: Created sequence[] and round variables and modified getRandomSquare function to push square value to sequence, increment round and update instructions. CSS: Moved font-family to body.

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,45 @@
 'use strict';
 
-$(document).ready(function () {
+// $(document).ready(function () {
 	var pressPlay = $('#play');
 	var instructions = $('#instructions');
 	var red = $('#red');
 	var yellow = $('#yellow');
 	var green = $('#green');
 	var blue = $('#blue');
+	var squares = $('.squares');
+	var control = $('#control')
+	var sequence = [];
+	var round = 0;
 
 	function getRandomSquare() {
 		var index = Math.floor(Math.random() * 4);
 		if (index == 0) {
 			red.fadeOut(250).fadeIn(250);
+			sequence.push(index);
 		} else if (index == 1) {
 			yellow.fadeOut(250).fadeIn(250);
+			sequence.push(index);
 		} else if (index == 2) {
 			green.fadeOut(250).fadeIn(250);
+			sequence.push(index);
 		} else if (index == 3) {
 			blue.fadeOut(250).fadeIn(250);
+			sequence.push(index);
 		}
+		round++;
+		instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + round + '</h3>');
+		$('#play').attr('hidden', true);
 	}
 	
+	squares.click(function() {
+
+	});
+
 	pressPlay.click(function() {
 		instructions.html('Watch carefully.');
 		setTimeout(function() {
 			getRandomSquare();
-			instructions.html('Select the tiles that reproduce the sequence.');
 		}, 1000);
 	});
-});
+// });
