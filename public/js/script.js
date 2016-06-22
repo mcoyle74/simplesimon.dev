6eac0bba254ee6b87eb72624a415c8ce9e404236
Michael Coyle <mcoyle74@gmail.com>
2016-06-22 15:43:20 -0500

    Removed 'if' statement from getRandomTile() because it was unnecessary.

--- public/js/script.js
+++ public/js/script.js
@@ -3,23 +3,30 @@
 // $(document).ready(function () {
 	var pressPlay = $('#play');
 	var instructions = $('#instructions');
-	var red = $('#red');
-	var yellow = $('#yellow');
-	var green = $('#green');
-	var blue = $('#blue');
-	var tiles = $('.tiles');
+	var red = $('[data-id="1"]');
+	var yellow = $('[data-id="2"]');
+	var green = $('[data-id="3"]');
+	var blue = $('[data-id="4"]');
+	var tiles = $('[data-id]');
 	var control = $('#control');
 
 	var simon = {
 		sequence: [],
 		round: 0,
 		animateSequence: function() {
-			// animate
+			setTimeout(function() {
+				simon.sequence.forEach(function(element, index) {
+					var iteration = $('[data-id="' + element + '"]');
+					iteration.fadeOut(1000).fadeIn(1000);
+				});
+			}, 1000)
 		},
 		check: function() {
 			player.sequence.forEach(function(color, index) {
 				if (color == simon.sequence[index]) {
 					console.log('success');
+					getRandomTile();
+					simon.round++;
 				} else {
 					console.log('failure')
 				}
@@ -29,25 +36,14 @@
 
 	var player = {
 		sequence: [],
-		rounds: 0,
+		rounds: 0
 	}	
 
 	function getRandomTile() {
 		var randomColor = Math.floor(Math.random() * 4) + 1;
-		if (randomColor == 1) {
-			red.fadeOut(250).fadeIn(250);
-			simon.sequence.push(randomColor);
-		} else if (randomColor == 2) {
-			yellow.fadeOut(250).fadeIn(250);
-			simon.sequence.push(randomColor);
-		} else if (randomColor == 3) {
-			green.fadeOut(250).fadeIn(250);
-			simon.sequence.push(randomColor);
-		} else if (randomColor == 4) {
-			blue.fadeOut(250).fadeIn(250);
-			simon.sequence.push(randomColor);
-		}
+		simon.sequence.push(randomColor);
 		simon.round++;
+		simon.animateSequence();
 		instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + simon.round + '</h3>');
 		pressPlay.attr('hidden', true);
 	}
