0d0c0f416f91993378c920ab2fb671a2a8bf5883
Michael Coyle <mcoyle74@gmail.com>
2016-06-23 10:56:00 -0500

    Changed getRandomTile() into simon object and moved gameOver out of simon and reorganized start(), playerGo(), etc.

--- public/js/script.js
+++ public/js/script.js
@@ -15,6 +15,15 @@
 	var simon = {
 		sequence: [],
 		round: 0,
+		getRandomTile: function() {
+			disablePlayer();
+			var randomColor = Math.floor(Math.random() * 4) + 1;
+			simon.sequence.push(randomColor);
+			simon.round++;
+			simon.animateSequence();
+			instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + simon.round + '</h3>');
+			pressPlay.attr('hidden', true);
+		}, 
 		animateSequence: function() {
 			setTimeout(function() {
 				simon.sequence.forEach(function(element, index) {
@@ -25,25 +34,20 @@
 				});
 			}, 500);
 			playerGo();
-		},
-		gameOver: function() {
-			instructions.html('Game Over');
-			setTimeout(function(){
-				start();
-			}, 3000)
 		}
 	}	
 
-	function getRandomTile() {
-		disablePlayer();
-		var randomColor = Math.floor(Math.random() * 4) + 1;
-		simon.sequence.push(randomColor);
-		simon.round++;
-		simon.animateSequence();
-		instructions.html('Select the tiles that reproduce the sequence.').append('<h3>Round: ' + simon.round + '</h3>');
-		pressPlay.attr('hidden', true);
+	function start() {
+		instructions.html('Press <span class="italics">Play</span> to begin.');
+		pressPlay.attr('hidden', false);
+		pressPlay.click(function() {
+		instructions.html('Watch carefully.');
+		setTimeout(function() {
+			simon.getRandomTile();
+		}, 1500);
+	});
 	}
-	
+
 	function playerGo() {
 		tiles.click(function(event) {
 		var tilePressed = $(this).data('id');
@@ -52,29 +56,26 @@
 			index++;
 		} else {
 			index = 0;
-			simon.gameOver();
+			gameOver();
 		}
 		if (index == simon.sequence.length) {
 			index = 0;
 			instructions.html('Watch carefully.');
-			getRandomTile();
+			simon.getRandomTile();
 		}
-	});
-	}
-
-	function start() {
-		instructions.html('Press <span class="italics">Play</span> to begin.');
-		pressPlay.attr('hidden', false);
-		pressPlay.click(function() {
-		instructions.html('Watch carefully.');
-		setTimeout(function() {
-			getRandomTile();
-		}, 1500);
 	});
 	}
 
 	function disablePlayer() {
 		tiles.off('click');
+		instructions.html('Watch carefully.');
+	}
+
+	function gameOver() {
+		instructions.html('Game Over');
+		setTimeout(function(){
+			start();
+		}, 3000);
 	}
 
 	disablePlayer();
